70658052ff1042fa6b37dbffa96c60ba63cf13c7
Gabriel Torello <gabriel128@gmail.com>
2021-05-31 20:49:07 +1000

    Add Bst

--- src/Bst.hs
+++ src/Bst.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE DatatypeContexts #-}
-
 {-# LANGUAGE InstanceSigs #-}
 module Bst where
 
@@ -10,3 +8,7 @@ instance Functor Tree where
   fmap :: (a -> b) -> Tree a -> Tree b
   fmap _ Leaf = Leaf
   fmap f (Node ltree x rtree) = Node (fmap f ltree) (f x) (fmap f rtree)
+
+inOrderT :: Tree a -> [a]
+inOrderT Leaf = []
+inOrderT (Node ltree x rtree) = inOrderT ltree ++ [x] ++ inOrderT rtree
--- src/sets/SetImpls.hs
+++ src/sets/SetImpls.hs
@@ -8,22 +8,29 @@ module SetImpls where
 import Set
 import Bst
 
-type UnbalancedSet = Tree
+type UnbalancedTreeSet = Tree
 
-instance (Ord a) => Set (UnbalancedSet a) a where
-  empty :: Ord a => UnbalancedSet a
+instance (Ord a) => Set (UnbalancedTreeSet a) a where
+  empty :: Ord a => UnbalancedTreeSet a
   empty = Leaf
 
-  insert :: Ord a => a -> UnbalancedSet a -> UnbalancedSet a
+  -- | Inserts a into the tree
+  --
+  -- Examples: (Should fail)
+  --
+  -- >>> Set.insert 3 . Set.insert 5 . Set.insert 4 $ Set.new :: UnbalancedTreeSet Int
+  -- [2]
+  insert :: Ord a => a -> UnbalancedTreeSet a -> UnbalancedTreeSet a
   insert x Leaf = Node Leaf x Leaf
   insert x node@(Node ltree y rtree)
     | x < y = Node (insert x ltree) y rtree
     | x > y = Node ltree y (insert x rtree)
     | x == y = node
 
-  member :: Ord a => a -> UnbalancedSet a -> Bool
+  member :: Ord a => a -> UnbalancedTreeSet a -> Bool
   member x Leaf = False
   member x node@(Node ltree y rtree)
     | x < y = member x ltree
     | x > y = member x rtree
     | x == y = True
+
